571459b55dc71cb0f4e7a089580614466caed1d8
podlizka <n_liza@icloud.com>
2024-11-07 23:50:11 +0500

    iuiu

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -28,10 +28,10 @@ class Category(PublishModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text=(
+        help_text=
             'Идентификатор страницы для URL;'
             'разрешены символы, латиницы, цифры, дефис и подчёркивание.'
-        ))
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -60,10 +60,10 @@ class Post(PublishModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text=(
+        help_text=
             'Если установить дату и время в будущем — '
             'можно делать отложенные публикации.'
-        ))
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
