34b001fc33c7eb21b6f4c820bf366852443b41bd
podlizka <n_liza@icloud.com>
2024-11-08 00:12:49 +0500

    itogstopudov

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -6,6 +6,7 @@ admin.site.register(Category)
 admin.site.register(Location)
 admin.site.register(Post)
 
+
 class PostAdmin (admin.ModelAdmin):
     list_display = ('title', 'category', 'location', 'author', 'created_at')
     ist_filter = ('category', 'location', 'created_at')
