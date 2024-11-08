14d3c461c67213c48fc47d2470c89a1a3f2b73c1
podlizka <n_liza@icloud.com>
2024-11-08 17:42:39 +0500

    vcee

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -5,8 +5,10 @@ from .models import Category, Location, Post
 admin.site.register(Category)
 admin.site.register(Location)
 
+
 class PostAdmin (admin.ModelAdmin):
     list_display = ('title', 'category', 'location', 'author', 'created_at')
     ist_filter = ('category', 'location', 'created_at')
     search_fields = ['title']
 admin.site.register(Post, PostAdmin)
+
